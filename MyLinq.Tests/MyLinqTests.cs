e15aa2a95136ce265a17f61a41a6d93e10e03f90
Unaro <vladislav.burado@gmail.com>
2023-11-18 10:02:34 +0600

    Добавьте файлы проекта.

---
+++ MyLinq.Tests/MyLinqTests.cs
@@ -0,0 +1,56 @@
+namespace MyLinq.Tests
+{
+    public class WhereTests
+    {
+        [Fact]
+        public void NotEmptyCollection_GetElementsMoreThanFive_Success()
+        {
+            //Arrange.
+            var array = new[] { 1, 2, 3, 5, 6, 12, 5, 22 };
+
+            var expected = new[] { 6, 12, 22 };
+
+            var result = array.Where(x => x > 5);
+
+
+            Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        public void NullSource_Throws()
+        {
+            int[] array1 = null;
+
+            Assert.Throws<InvalidOperationException>(() => array1.Where(x => x > 0));
+        }
+
+        [Fact]
+        public void NotEmptyCollection_GetNonFeetElement_EmptyResult()
+        {
+            var array = new[] { 1, 2, 3, 5, 6, 12, 5, 22 };
+
+            var result = array.Where(x => x == -5);
+
+            Assert.Empty(result);
+
+
+        }
+
+        [Fact]
+        public void NotEmptyCollection_GradeTen_Single()
+        {
+            var array = new (int Grade, double Salary)[]
+            {
+                (Grade: 5, Salary: 1200),
+                (Grade: 10, Salary: 2400)
+            };
+
+
+            var result = array.Where(x => x.Grade == 10);
+
+            Assert.Single(result);
+
+
+        }
+    }
+}
---
+++ MyLinq/MyLinqExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyLinq
+{
+    public static class MyLinqExtension
+    {
+        public static TSource[] Where<TSource>(this TSource[] values, Predicate<TSource> predicate)
+        {
+            if (values == null) throw new InvalidOperationException(nameof(values));
+            List<TSource> res = new();
+
+            foreach (TSource value in values)
+            {
+                if (predicate.Invoke(value)) res.Add(value);
+            }
+
+            return res.ToArray();
+
+        } 
+    }
+}
